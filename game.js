a07b35222f338621c717da89424a486d7849341f
AlyAssem <alyassem1997@gmail.com>
2020-12-05 16:44:32 +0300

    first commit

---
+++ game.js
@@ -0,0 +1,82 @@
+var buttonColours = ['red', 'blue', 'green', 'yellow'];
+
+var gamePattern = [];
+
+var userClickedPattern = [];
+
+var gameStarted = false;
+
+var level = 0;
+
+$(document).keypress(function () {
+  if (gameStarted == false) {
+    nextSequence();
+    gameStarted = true;
+  }
+});
+
+$('.btn').on('click', function () {
+  var userChosenColour = $(this).attr('id');
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+function nextSequence() {
+  level++;
+  $('#level-title').text('Level ' + level);
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $('#' + randomChosenColour)
+    .fadeOut(100)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+
+  playSound(randomChosenColour);
+}
+
+function playSound(name) {
+  var audio = new Audio('sounds/' + name + '.mp3');
+  audio.play();
+}
+
+function animatePress(currentColour) {
+  $('.' + currentColour).addClass('pressed');
+  setTimeout(function () {
+    $('.' + currentColour).removeClass('pressed');
+  }, 100);
+}
+
+function checkAnswer(currentLevel) {
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+      userClickedPattern = [];
+    }
+  } else {
+    var audio = new Audio('sounds/wrong.mp3');
+    audio.play();
+    $('body').addClass('game-over');
+    setTimeout(function () {
+      $('body').removeClass('game-over');
+    }, 1000);
+    $('h1').text('Game Over, Press Any Key to Restart');
+
+    // restart the game.
+    startOver();
+  }
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  gameStarted = false;
+}
